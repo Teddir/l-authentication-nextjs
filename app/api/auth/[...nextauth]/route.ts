25b2c46ba9cda66ea214cec710c63ae5d1e6e971
Teddir <ferryaja20000@gmail.com>
2024-04-03 11:35:45 +0700

    fix: new update

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import PinterestProvider from "next-auth/providers/pinterest";
+
+const handler = NextAuth({
+  // Secret for Next-auth, without this JWT encryption/decryption won't work
+  secret: process.env.NEXTAUTH_SECRET,
+
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    PinterestProvider({
+      clientId: process.env.PINTEREST_ID as string,
+      clientSecret: process.env.PINTEREST_SECRET as string,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
+  },
+  env: {
+    GOOGLE_CLIENT_ID:
+      "905023012050-c3ilcevq8df1acoesrk7oa8purefbs6j.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
+    PINTEREST_ID: "",
+    PINTEREST_SECRET: "",
+    NEXTAUTH_SECRET: "hRIMXFxrnKEKzRsMY8dAkegqynEbMUmbp+79UC+800Q=",
+    NEXTAUTH_URL: "http://localhost:3000",
+  },
+};
 
 export default nextConfig;

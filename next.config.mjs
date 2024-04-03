1e3a2f0cc9366344b01c8f923f5b383720d1d45a
Teddir <ferryaja20000@gmail.com>
2024-04-03 11:46:46 +0700

    fix: new update

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,12 @@ const nextConfig = {
     ],
   },
   env: {
-    GOOGLE_CLIENT_ID:
-      "905023012050-c3ilcevq8df1acoesrk7oa8purefbs6j.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-    PINTEREST_ID: "",
-    PINTEREST_SECRET: "",
-    NEXTAUTH_SECRET: "hRIMXFxrnKEKzRsMY8dAkegqynEbMUmbp+79UC+800Q=",
-    NEXTAUTH_URL: "http://localhost:3000",
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    PINTEREST_ID: process.env.PINTEREST_ID,
+    PINTEREST_SECRET: process.env.PINTEREST_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 };
 
